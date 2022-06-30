ce536ff079c62c567bb120b9a65fab266a1e43cf
SomexJames <brandon021@gmail.com>
2022-06-30 14:24:10 -0600

    initialize about content

---
+++ src/components/AboutContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AboutContent.css";
+
+const AboutContent = (abt) => {
+    
+
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default AboutContent;
