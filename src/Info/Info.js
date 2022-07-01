994cb3eaf7e4d311bbdb36780c4a680a4ad58eea
SomexJames <brandon021@gmail.com>
2022-06-30 19:28:04 -0600

    Add "about" info in Info.js

---
+++ src/Info/Info.js
@@ -0,0 +1,14 @@
+const about = [
+    {
+        id: 1,
+        title: "",
+        description: ""
+    }, 
+    {
+        id: 2,
+        title: "",
+        description: ''
+    }
+]
+
+export default { about };
