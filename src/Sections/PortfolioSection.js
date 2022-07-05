19fcbdf0667db29e15ceae31afed979161a20183
SomexJames <brandon021@gmail.com>
2022-07-05 15:17:32 -0600

    initialize PortfolioSection

---
+++ src/Sections/PortfolioSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./PortfolioSection.css";
+import PortfolioContent from "../components/PortfolioContent";
+
+
+const PortfolioSection = () => {
+
+    return(
+        <section>
+            <div></div>
+        </section>
+    )
+}
+
+export default PortfolioSection;
