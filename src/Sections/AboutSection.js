ab26a826306e9eded3ec25fee5573a482fd37c6f
SomexJames <brandon021@gmail.com>
2022-07-05 13:39:20 -0600

    initialize AboutSection

---
+++ src/Sections/AboutSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AboutSection.css";
+import AboutContent from "../components/AboutContent";
+import AboutSkills from "../components/AboutSkills";
+
+
+const AboutSection = () => {
+
+    return(
+        <section>
+            <div></div>
+        </section>
+    )
+}
+
+export default AboutSection;
