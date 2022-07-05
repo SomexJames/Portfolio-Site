c8c86d4e5b2662d60e0e68c7c7b25e62a3bf8ecc
SomexJames <brandon021@gmail.com>
2022-07-05 14:49:10 -0600

    initialize HomeSection

---
+++ src/Sections/HomeSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomeSection.css";
+
+
+const HomeSection = () => {
+
+    return(
+        <section>
+            <div></div>
+        </section>
+    )
+}
+
+export default HomeSection;
