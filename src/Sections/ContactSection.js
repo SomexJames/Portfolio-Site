a426cc7f424e8dccc15168f26435d2b0ae8ab104
SomexJames <brandon021@gmail.com>
2022-07-05 14:03:50 -0600

    initialize ContactSection

---
+++ src/Sections/ContactSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ContactSection.css";
+import ContactContent from "../components/ContactContent";
+
+
+const ContactSection = () => {
+
+    return(
+        <section>
+            <div></div>
+        </section>
+    )
+}
+
+export default ContactSection;
