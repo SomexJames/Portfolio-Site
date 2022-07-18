d0c1006995e2f46e73ea348c91207df1c2b9aced
SomexJames <brandon021@gmail.com>
2022-07-18 16:52:13 -0600

    added PortfolioSection

--- src/Sections/PortfolioSection.js
+++ src/Sections/PortfolioSection.js
@@ -1,13 +1,31 @@
 import React from "react";
 import "./PortfolioSection.css";
 import PortfolioContent from "../components/PortfolioContent";
+import projects from "../Info/Info";
+import { RiArrowDownSLine } from "react-icons/ri";
 
 
 const PortfolioSection = () => {
 
     return(
-        <section>
-            <div></div>
+        <section id="portfolio">
+            <div className="section portfolio">
+                <div className="heading">
+                    <div className="section__title">Portfolio</div>
+                </div>
+                <div className="section__portfolio">
+                    {
+                        projects.projects.map((proj) =>
+                            <PortfolioContent
+                                proj={proj}
+                            />
+                        )
+                    }
+                </div>
+            </div>
+            <div className="navArrow">
+                <a href="#about"><RiArrowDownSLine /></a>
+            </div>
         </section>
     )
 }
