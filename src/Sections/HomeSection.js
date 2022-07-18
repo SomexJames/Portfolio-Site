755bc90e4356aec010189286e1e4732ea1375b42
SomexJames <brandon021@gmail.com>
2022-07-18 16:52:04 -0600

    added HomeSection

--- src/Sections/HomeSection.js
+++ src/Sections/HomeSection.js
@@ -1,12 +1,36 @@
 import React from "react";
 import "./HomeSection.css";
+import { FaGithub, FaLinkedin, FaFileAlt } from "react-icons/fa";
+import { RiArrowDownSLine } from "react-icons/ri";
+import main from "../Info/Info";
 
 
 const HomeSection = () => {
 
     return(
-        <section>
-            <div></div>
+        <section id="home">
+            <div className="bg__image">
+                <div className="section__home">
+                    <div className="home__content">
+                        <div className="name">I am {main.main.name}.</div>
+                        <div className="description">{main.main.description}</div>
+                        <div className="socials">
+                            <div className="github">
+                                <a href="https://github.com/SomexJames" target="_blank"><FaGithub /></a>
+                            </div>
+                            <div className="linkedin">
+                                <a href="https://www.linkedin.com/in/bjkang17/" target="_blank"><FaLinkedin /></a>
+                            </div>
+                            <div className="filealt">
+                                <a href={main.main.resume} target="_blank"><FaFileAlt /></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="navArrow">
+                        <a href="#portfolio"><RiArrowDownSLine /></a>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
