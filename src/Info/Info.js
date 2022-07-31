fd5d5116e18bb70c0481172f07a1868a136bd6e8
SomexJames <brandon021@gmail.com>
2022-07-31 10:37:06 -0600

    import assets into Info.js

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -1,3 +1,10 @@
+import P1BG from "../assets/p1bg.png";
+import P2BG from "../assets/p2bg.png";
+import P3BG from "../assets/p3bg.png";
+import P4BG from "../assets/p4bg.png";
+import P5BG from "../assets/p5bg.png";
+
+
 const main = {
     name: "Brandon Kang",
     description: "Student at The University of Texas at Dallas",
