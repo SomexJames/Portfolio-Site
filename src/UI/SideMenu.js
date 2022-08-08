17a33f60841ece1d5ac3069aa93bcb6c54351cd2
SomexJames <brandon021@gmail.com>
2022-08-08 14:13:54 -0600

    added SideMenu

--- src/UI/SideMenu.js
+++ src/UI/SideMenu.js
@@ -1,13 +1,51 @@
-import React from "react";
-import "./SideMenu.css";
-
+import React, { useState } from 'react';
+import './SideMenu.css';
+import ScreenSize from "../Hooks/ScreenSize";
+import Popup from "reactjs-popup";
+import { FaTimes } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const SideMenu = () => {
 
-    return(
-        <div>
-        </div>
-    )
+    const dimensions = ScreenSize();
+
+    var style = {
+        visibility: "hidden"
+    };
+
+    if (dimensions.width < 768) {
+        style = {
+            visibility: "visible"
+        };
+    }
+
+    const [open, setOpen] = useState(false);
+
+    const closeModal = () => setOpen(false);
+
+    return (
+        <>
+            <button type="button" style={style} className="sidemenu__button" onClick={() => setOpen(o => !o)}>
+                <GiHamburgerMenu />
+            </button>
+            <div className="sidemenu">
+                <Popup className="sidemenu-popup" open={open} closeOnDocumentClick onClose={closeModal}>
+                    <div className="modal">
+                        <a className="sidemenu__close" onClick={closeModal}>
+                            <FaTimes />
+                        </a>
+                        <div className='sideMenu__content' onClick={() => setOpen(o => !o)}>
+                            <a href="#home">Home</a>
+                            <a href="#portfolio">Portfolio</a>
+                            <a href="#about">About</a>
+                            <a href="#contact">Contact</a>
+                        </div>
+                    </div>
+                </Popup>
+            </div>
+        </>
+  )
+
 }
 
-export default SideMenu;
+export default SideMenu
