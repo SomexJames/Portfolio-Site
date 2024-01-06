9ca1db81a9a4506d429e80711327a2a32ffb05e4
SomexJames <93785036+SomexJames@users.noreply.github.com>
2024-01-06 11:14:20 -0700

    changed css to improve NavArrow placement

--- src/Sections/HomeSection.js
+++ src/Sections/HomeSection.js
@@ -22,24 +22,26 @@ const HomeSection = () => {
         <section id="home">
             <div className="bg__image">
                 <div className="section__home">
-                    <div className="home__content">
-                        <div className="name">I am {main.main.name}.</div>
-                        <div className="description">{main.main.description}</div>
-                        <div className="line">{"_".repeat(lineRepeat)}</div>
-                        <div className="socials">
-                            <div className="github">
-                                <a href="https://github.com/SomexJames" target="_blank"><FaGithub /></a>
-                            </div>
-                            <div className="linkedin">
-                                <a href="https://www.linkedin.com/in/bjkang17/" target="_blank"><FaLinkedin /></a>
-                            </div>
-                            <div className="filealt">
-                                <a href={main.main.resume} target="_blank"><FaFileAlt /></a>
+                    <div className="home__container">
+                        <div className="home__content">
+                            <div className="name">I am {main.main.name}.</div>
+                            <div className="description">{main.main.description}</div>
+                            <div className="line">{"_".repeat(lineRepeat)}</div>
+                            <div className="socials">
+                                <div className="github">
+                                    <a href="https://github.com/SomexJames" target="_blank"><FaGithub /></a>
+                                </div>
+                                <div className="linkedin">
+                                    <a href="https://www.linkedin.com/in/bjkang17/" target="_blank"><FaLinkedin /></a>
+                                </div>
+                                <div className="filealt">
+                                    <a href={main.main.resume} target="_blank"><FaFileAlt /></a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="navArrow">
-                        <a href="#portfolio"><RiArrowDownSLine /></a>
+                        <div className="navArrow">
+                            <a href="#portfolio"><RiArrowDownSLine /></a>
+                        </div>
                     </div>
                 </div>
             </div>
